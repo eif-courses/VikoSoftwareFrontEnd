97b24cc28b74faccfd5965826914f53a0d20f0a3
Marius <m.gzegozevskis@gmail.com>
2024-09-10 21:09:25 +0300

    v2.0 app

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,16 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  $END$
+<UContainer>
+<UCard class="mt-10">
+  <template #header>
+    <div class="flex justify-between">
+      <h1>Vilniaus kolegija </h1>
+      <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
+    </div>
+  </template>
+  <slot/>
+
+</UCard>
+</UContainer>
 </template>
-
-<style scoped>
-
-</style>
+ <script setup lang="ts">
+ </script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  $END$
+<LoginForm/>
 </template>
 
 <style scoped>
